afb0378c91402cea22fe1e84ebb53b8fd4cd1e19
Greg Burd <gregburd@amazon.com>
2026-03-04 11:47:11 -0500

    Revert "Fix split count assertion: correct destination chunk start position"
    This reverts commit 915a1cdc9728ae03ed8fe8c170739a45d614efe8.

--- src/sparsemap.c
+++ src/sparsemap.c
@@ -3252,7 +3252,7 @@ sparsemap_split(sparsemap_t *map, sparsemap_idx_t idx, sparsemap_t *other)
     }
 
     /* Copy the bits in the sparse chunk, at most SM_CHUNK_MAX_CAPACITY. */
-    *(__sm_idx_t *)dst = idx;
+    *(__sm_idx_t *)dst = src_start;
     for (size_t j = idx; j < src_start + SM_CHUNK_MAX_CAPACITY; j++) {
       if (sparsemap_is_set(map, j)) {
         __sm_map_set(other, j, false);
